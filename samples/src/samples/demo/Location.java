d835dd2ccf35cf7fbf00cd0ab6aea84c2d617f6e
Habeeb E Sadeed <hsadeed9@gmail.com>
2021-03-18 23:32:16 +0800

    File Commited is Location

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,32 @@
+package samples.demo;
+
+public class Location {
+
+	
+	private String city;
+	private int FlatNo;
+	
+	
+	public void setCity(String city) {
+		
+		this.city=city;
+	}
+	
+	public  String getCity() {
+		
+		return city;
+	}
+	
+	
+public void setFlatNo(int FlatNo) {
+		
+		this.FlatNo=FlatNo;
+	}
+	
+	public  int getFlatNo() {
+		
+		return FlatNo;
+	}
+	
+	
+}
